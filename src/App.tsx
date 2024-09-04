12ec4ece7fd02dc203c172e6db32d844ebbe899d
Asadbek Abduvoitov <161235406+asadbekabduvoitov-nordra@users.noreply.github.com>
2024-09-04 08:35:34 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+	return (
+		<div className="flex w-full h-screen items-center justify-center">
+			<Button variant="link">Hello</Button>
+		</div>
+	);
+}
+
+export default App;
